16a96c8a1cea74b0d0717ae4b812e5e532bc8739
bublik13 <f9531787658@yandex.ru>
2022-08-22 22:29:10 +0300

    mobile

--- app/js/main.js
+++ app/js/main.js
@@ -2,14 +2,33 @@ import {testFunction} from './scripts/test';
 testFunction();
 
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    // spaceBetween: 40,
-    loop: true,
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    }
-  });
-  
+  // Optional parameters
+  direction: 'horizontal',
+  // spaceBetween: 40,
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+});
+const swiperTwo = new Swiper('.swiper-mobile', {
+  // Optional parameters
+  direction: 'horizontal',
+  spaceBetween: 40,
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+});
+const body = document.querySelector('.js-body');
+const nav = document.querySelector('.js-nav');
+const burger = document.querySelector('.js-burger');
+
+burger.addEventListener('click', ()=>{  
+  burger.classList.toggle('burger--active');
+  nav.classList.toggle('header__nav--active');
+  body.classList.toggle('body--lock');
+});
